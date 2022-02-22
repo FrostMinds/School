a13739d3e65447b9fbbe71521bddf16f28fa7a99
FrostMind <Priimak98@Gmail.com>
2022-02-22 18:54:00 +1000

    Initial commit

---
+++ src/test/java/Homewrok/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package Homewrok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
