8b58c1b13cfe2b3d222aa2b5b700cbeffee39041
FrostMind <Priimak98@Gmail.com>
2022-03-02 15:13:58 +1000

    Добавил скрипты

---
+++ scripts.sql
@@ -0,0 +1,5 @@
+select * from student where age >= 8 and age <= 12;
+select name from student;
+select * from student where name like '%с%';
+select * from student where age < student.id;
+select * from student order by age;
